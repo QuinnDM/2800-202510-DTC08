6f152c9ba8fe170cce0e2636f005fc1af2e12ad5
mason young <masonyoung1997@gmail.com>
2025-05-02 12:09:58 -0700

    Prevent users from panning out of bounds

--- public/scripts/explore.js
+++ public/scripts/explore.js
@@ -3,6 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
     map.zoomControl.setPosition('topright');
 
+    // Set the map bounds
+    const south_west = L.latLng(-90, -200)
+    const north_east = L.latLng(90, 200)
+    const bounds = L.latLngBounds(south_west, north_east)
+
+    // Prevent users from being able to pan outside of the map's bounds
+    /* credit to rob.m's stackflow answer to prevent user from being able to pan out of map bounds
+    https://stackoverflow.com/questions/22155017/can-i-prevent-panning-leaflet-map-out-of-the-worlds-edge*/
+    map.setMaxBounds(bounds);
+    map.on('drag', function () {
+        map.panInsideBounds(bounds, { animate: false });
+    })
+
     L.tileLayer("/tiles/{z}/{x}/{y}", {
         attribution: 'Tiles &copy; <a href="https://jawg.io">Jawg</a>, &copy; OpenStreetMap contributors',
         minZoom: 3, 
